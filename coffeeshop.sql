64b0b719b720f65810817e1d0b4510443698ee56
Ahmed <memorykart1@gmail.com>
2021-05-29 09:36:12 +0200

    database init

---
+++ coffeeshop.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 29, 2021 at 09:33 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coffeeshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beans`
+--
+
+CREATE TABLE `beans` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `condiment`
+--
+
+CREATE TABLE `condiment` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` double(10,2) NOT NULL,
+  `type` enum('sauce','sweetener','creamer','milk') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drink`
+--
+
+CREATE TABLE `drink` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `condiments_ID` text NOT NULL,
+  `beans_ID` int(11) NOT NULL,
+  `grind` enum('coarse','medium-coarse','medium','fine','extra fine') NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `username` int(255) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mobile` varchar(11) NOT NULL,
+  `address` text NOT NULL,
+  `type` enum('admin','employee','customer','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `beans`
+--
+ALTER TABLE `beans`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `condiment`
+--
+ALTER TABLE `condiment`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `drink`
+--
+ALTER TABLE `drink`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `beans` (`beans_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `beans`
+--
+ALTER TABLE `beans`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `condiment`
+--
+ALTER TABLE `condiment`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `drink`
+--
+ALTER TABLE `drink`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `drink`
+--
+ALTER TABLE `drink`
+  ADD CONSTRAINT `drink_ibfk_1` FOREIGN KEY (`beans_ID`) REFERENCES `beans` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
